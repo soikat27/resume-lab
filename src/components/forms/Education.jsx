219379a653255c660c9ce3d49f88b5f8c945a780
Soikat <193371447+soikat27@users.noreply.github.com>
2026-08-21 02:36:15 -0400

    Feat: Education form interface allows to add a "EducationChunk" - a new education form on button click
    -- each education has a delete button - reddish themed

--- src/components/forms/Education.jsx
+++ src/components/forms/Education.jsx
@@ -1,15 +1,33 @@
-import InputField from "./utils/InputField.jsx";
+import EducationChunk from "./utils/EducationChunk.jsx";
 import ButtonGroup from "./utils/ButtonGroup.jsx";
+import { useState } from "react";
 
-export default function Education ({addResumeInfo, ...rest}) {
+export default function Education ({updateOnNext, ...rest}) {
+    const [educationIds, setEducationIds] = useState([]);
+
+    function nextForm() {
+        
+    }
+
+    function addEducation() {
+        setEducationIds(previous => [...previous, crypto.randomUUID()]);
+    }
+
+    function deleteEducation(id) {
+        const newIds = educationIds.filter(eduId => eduId !== id);
+        setEducationIds(newIds);
+    }
+
     return (
-        <form className="container education" onSubmit={addResumeInfo}>
-            <h3>Education</h3>
-
-            <InputField type="text" name="Institution Name" />
-            <InputField type="text" name="Degree" />
-            <InputField type="date" name="Start Date" />
-            <InputField type="date" name="End Date" />
+        <form className="container education" onSubmit={nextForm}>
+            <div className="form-title">
+                <h3>Education</h3>
+                <button type="button" className="btn-add" onClick={addEducation}>+</button>
+            </div>
+            
+            {educationIds.map((EducationId, index) => (
+                <EducationChunk key={EducationId} id={EducationId} count={index + 1} deleteEducation={deleteEducation}/>
+            ))}
 
             <ButtonGroup {...rest}/>
         </form>
