17e81a8f33403551d0a1eff3ab5d1b03abb2fca4
Soikat <193371447+soikat27@users.noreply.github.com>
2026-08-26 02:28:37 -0400

    Feat: application now allows to load data
    -- caution: must be a valid JSON file (generated from this resumeLab application)

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -17,14 +17,38 @@ export default function Body({formInfo, goToNextForm, goToPrevForm, resetFormInd
         setFormKey(previous => previous+1);
     }
 
+    function handleLoadResume(event) {
+        // 1. get the file 
+        const file = event.target.files[0];
+
+        // 2. read the file
+        const reader = new FileReader();
+        reader.onload = () => {
+            const resumeJSON = reader.result;
+            const resumeData = JSON.parse(resumeJSON);
+
+            setResumeData(resumeData);
+
+            // reset form index
+            resetFormIndex();
+            setFormKey(previous => previous+1);
+        }
+        reader.readAsText(file);
+    }
+
     return (
         <main>
             <div className="main-title">
                 <div className="title">
                     <h2>Build your resume in {formInfo.totalForms} steps.</h2>
-                    <button type="button" className="btn-secondary" onClick={handleAutofill} >Autofill</button>
+                    <div className="buttons">
+                        <button type="button" className="btn-secondary" onClick={handleAutofill}>Autofill</button>
+                        <input type="file" accept=".json" onChange={handleLoadResume} />
+                    </div>
+                    
                 </div>
                 <h4>Fill each section, preview on the right, download when you're done. <span className="jake-resume">Inspired from <a href="https://github.com/jakegut/resume">Jake's resume</a>.</span></h4>
+                <p><span>Caution: </span>You must use a valid JSON file(generated from <span>resumeLab</span>)  while loading a saved resume data.</p>
             </div>
             <div className="main-body">
                 <div className="form">
